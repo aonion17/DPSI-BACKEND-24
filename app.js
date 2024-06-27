d40ca8bd30fe311eac5765c4a47c9036a69386d0
Deka Marella <deka.marella@gmail.com>
2024-06-27 11:41:05 +0700

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -70,12 +69,10 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
